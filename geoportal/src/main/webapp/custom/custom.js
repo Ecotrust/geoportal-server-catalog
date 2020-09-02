0a6d73225f600d3767beb04ef0d76c545f2e2e34
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-09-02 13:28:30 -0700

    enable catalog users to explode the abstracts

--- geoportal/src/main/webapp/custom/custom.js
+++ geoportal/src/main/webapp/custom/custom.js
@@ -1,3 +1,32 @@
 (function() {
   // Custom initialization
 })();
+
+var descriptionClickEvent = function() {
+  if ($(this).hasClass('explode')) {
+    $(this).removeClass('explode');
+  } else {
+    $(this).addClass('explode');
+  }
+}
+
+var updateDescriptionClickEvents = function() {
+  $('.g-item-description').off("click");
+  $('.g-item-description').on("click", descriptionClickEvent);
+}
+
+var oneSecondRepeater = function() {
+  updateDescriptionClickEvents();
+
+  setTimeout(oneSecondRepeater, 1000);
+}
+
+onCatalogLoad = function() {
+  oneSecondRepeater();
+}
+
+$(document).ready(function() {
+  setTimeout(function(){
+    onCatalogLoad();
+  },1200);
+});
